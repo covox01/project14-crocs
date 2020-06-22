99f71e1d1a8c101ffe637b19e7bce638016f2414
Dennis <oba.dennis@gmail.com>
2020-06-21 21:08:27 -0400

    changing the loop

--- js/main.js
+++ js/main.js
@@ -6,17 +6,22 @@ var copyValue = document.querySelector("#text-c")
 
 function start(){
    var tl = new TimelineMax({})
-
    gsap.set(".copy", { transformOrigin: "center center", scale: 1, opacity: 1})
+   gsap.set("#inner-ring", {transformOrigin: "center center", scale: .8})
+   gsap.set("#outer-ring", { transformOrigin: "center center", scale: .7 })
    gsap.set("#croc", { transformOrigin: "center center", x: 10, y: 200, rotation: 0, opacity: .3})
-   gsap.set("#clip-1", { y: 10})
+   gsap.set("#clip-1", { y: 10, scale: .8, transformOrigin: "center center"})
+   gsap.set("#circleClip", {scale: .8, transformOrigin: "center center" })
 
-   tl.to("#croc", .4, {  y: -05, rotation: 2, opacity: 1, ease: crocEase1})
+   tl
+   .to("#outer-ring, #inner-ring, #clip-1, #circleClip", .5, {scale: 1, ease: Power3.easeInOut})
+   .to("#outer-ring", .5, { scale: 1, ease: Power3.easeInOut }, "-=.45")
+   .to("#croc", .4, {  y: -05, rotation: 2, opacity: 1, ease: crocEase1}, "-=.3")
    .to("#croc", .3, {rotation: 0, x: 0, y: 0, ease: crocEase2}, "sync")
    .to("#clip-1", .2, {y: 30, opacity: 0}, "sync")
    .staggerFrom(".copy", t, { opacity: 0, scale: .5, ease: copyEase }, .05, "-=.6")
 
-   gsap.delayedCall(1.3, clearProp)
+   gsap.delayedCall(2.5, clearProp)
 }
 
 function clearProp(){
@@ -39,13 +44,17 @@ function end(){
    tl.to("#croc", .2, {y: -7, rotation: 0, ease: crocEase1})
    .to("#croc", .35, {x: -40, y: 160, rotation: -20, opacity: .5, ease: crocEase1})
    .to("#clip-1", .2, { y: 0, opacity: 1 }, "-=.35")
-   .staggerTo(".copy", t, {opacity: 0, scale: .6, ease: copyEase}, .05, "-=.3")
+   .staggerTo(".copy", t, {opacity: 0, scale: .6, ease: copyEase}, .05, "-=.4")
+   .to("#outer-ring", .5, { scale: .7, ease: Power3.easeInOut, opacity: 0}, "-=.6")
+   .to("#inner-ring, #clip-1, #circleClip", .3, { scale: .8, ease: Power3.easeInOut }, "-=.45")
 
-   gsap.delayedCall(1, set)
+   tl.timeScale(.8)
+   gsap.delayedCall(1.3, set)
 }
 
 function set(){
    gsap.set(".copy", { transformOrigin: "center center", clearProps: "scale"})
+   gsap.set("#outer-ring", { opacity:1 })
    gsap.set("#croc", { transformOrigin: "center center", scale: 1, x: 0, y: 0, rotation: 0, opacity: 0, clearProps: "transform"})
    gsap.set("#clip-1", { y: 0, clearProps: "transform"})
 
